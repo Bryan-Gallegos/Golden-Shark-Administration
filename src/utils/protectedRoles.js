8e82bc97e7a19d547adc30a204fafdaeb9128493
Bryan Gallegos <64101108+Bryan-Gallegos@users.noreply.github.com>
2025-02-24 17:23:46 -0500

    changes, api

--- src/utils/protectedRoles.js
+++
@@ -1,9 +0,0 @@
-const protectedRoles = {
-    "administrator": "Administrador",
-    "author": "Autor",
-    "contributor": "Colaborador",
-    "editor": "Editor Página Web",
-    "subscriber": "Usuario Golden",
-};
-  export default protectedRoles;
-  
